7aadac16aacb6be37fef5bc472f7c0bb6a29dca0
nim3iri <musabsd1981@.com>
2023-09-07 11:31:45 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
- 
-int main()
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    printf("Hello Geek!");
-    return 0;
+    printf("with proper care, a dog is mans best friend,\n");
+    return (0);
 }
