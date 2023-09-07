01eb6e57abb78f2c51cde87deff41d2b97b68fe4
nim3iri <musabsd1981@.com>
2023-09-07 11:34:30 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,160 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
+ * Description: Calls puts and recieves value returned by puts after its
+ * execution.
+ * Return: Always return 0 to indicate success
+ */
+int main(void)
+{
+    puts("\"ALX is the best");
+    return (0);
+}
+Let's break down the code:
+
+int main(void) - This is the prototype and it is simply the entry point of the program.
+
+The second line puts("\"Programming is the best"); uses the puts function to output the message "Programming is the best to the console. The backslash before the double quote is an escape character that tells the compiler to treat the double quote as part of the string rather than the end of the string.
+
+The third line return (0); ends the main function and returns a value of zero to the operating system, indicating that the program executed successfully.
+
+Task 5. Hello, printf
+Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
+
+Use the function printf
+
+You are not allowed to use the function puts
+
+Your program should return 0
+
+Your program should compile without warning when using the -Wall gcc option.
+
+This problem is similar to Task 4 except that we are permitted to use printfas opposed to putsin the previous problem. I will use a similar sentence (with proper care, a dog is mans best friend,)as the one in the problem to explain.
+
+
+COPY
+
+COPY
+#include <stdio.h>
+
+/**
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzls");
-	return (0);
+    printf("with proper care, a dog is mans best friend,\n");
+    return (0);
 }
+The key thing to note here is that the newline is printed by adding the sequence "\n" at the tail end. This would not be necessary had we used the puts function instead. Read on the differences between puts and printf here.
+
+Task 6. Size is not grandeur, and territory does not make a nation
+Write a C program that prints the size of various types on the computer it is compiled and run on.
+
+You should produce the exact same output as in the example
+
+Warnings are allowed
+
+Your program should return 0
+
+You might have to install the package libc6-dev-i386 on your Linux to test the -m32 gcc option
+
+Task 7. Intel
+Write a script that generates the assembly code (Intel syntax) of a C code and save it in an output file.
+
+The C file name will be saved in the variable $CFILE.
+
+The output file should be named the same as the C file, but with the extension .s instead of .c.
+
+Example: if the C file is main.c, the output file should be main.s
+This problem is similar to Task 2 except that we are asked to make sure the assembly code output has intel syntax.
+
+Assuming you have GCC installed on your machine, you can generate the assembly code for a C file named "example.c" using the following command:
+
+
+COPY
+
+COPY
+gcc -S -masm=intel example.c -o example.s
+This command instructs GCC to generate the assembly code in Intel syntax ("-masm=intel") for the file "example.c" and save it in a file named "example.s" ("-o example.s").
+
+Here's a breakdown of the options used in the command:
+
+"-S": tells GCC to generate assembly code instead of object code
+
+"-masm=intel": The "-masm=intel" option is a compiler flag that is used to specify the assembly language syntax to be used by the GNU Compiler Collection (GCC) when generating assembly code.
+
+By default, GCC uses the AT&T syntax for assembly code, which is different from the Intel syntax. The Intel syntax is used by most other assemblers, including the Microsoft Assembler (MASM). The "-masm=intel" option instructs GCC to use the Intel syntax for the generated assembly code. This can be useful if you are working with code that was originally written for MASM or another assembler that uses the Intel syntax. To use this option, you would typically include it as part of the command line arguments when invoking GCC,
+
+"example.c": the name of the C file you want to compile
+
+"-o example.s": specifies the output file name for the generated assembly code
+
+You can run this command in a terminal or shell, and it will generate the assembly code for "example.c" and save it in "example.s". You can then open "example.s" with a text editor or any other tool to view the generated assembly code.
+
+Note that the generated assembly code may not be the most optimal or efficient version, so you may need to further optimize it by hand or using other tools.
+
+TASK 8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity
+Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+
+You are not allowed to use any functions listed in the NAME section of the man (3) printf or man (3) puts
+
+Your program should return 1
+
+Your program should compile without any warnings when using the -Wall gcc option.
+
+
+COPY
+
+COPY
+#include <stdio.h>
+/**
+ * main- Entry point
+ * @void: parameter
+ * Return: 1 (success)
+ **/
+int main(void)
+{
+    fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
+    return (1);
+}
+This is a preprocessor directive that includes the standard input/output library header file "stdio.h". This library provides functions for reading and writing data to and from files, as well as to the standard input and output streams.
+
+
+COPY
+
+COPY
+/**
+ * main- Entry point
+ * @void: parameter
+ * Return: 1 (success)
+ **/
+This is a documentation comment that explains the purpose of the "main" function. It states that the function is the entry point of the program and takes no arguments. It also specifies that the function returns an integer value of 1 to indicate success.
+
+
+COPY
+
+COPY
+int main(void)
+{
+    fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
+    return (1);
+}
+This is the implementation of the "main" function. The function uses the "fprintf" function to print the given message to the standard error stream. The message is enclosed in double quotes and includes an escaped double quote to represent the quote within the message. The backslash followed by a newline character (\n) is used to continue the message onto the next line for formatting purposes.
+
+Finally, the function returns an integer value of 1 to indicate success. Note that the return value is not necessary for this program, as it does not interact with other programs or scripts.
+
+SOME EXTRA NOTES ON printf(), puts(), and fprintf().
+
+All three functions are used to print data to the standard output stream, but they differ in their specific functionality:
+
+printf() and puts() are used to print data to the standard output stream, whereas fprintf() is used to print formatted data to a specified output stream.
+
+fprintf() is used to print formatted data to a specified output stream, such as a file or network socket. It takes the output stream as its first argument, followed by a format string and any additional arguments that correspond to the placeholders in the format string. The output is sent to the specified output stream, rather than the standard output stream.
+
+
+
+
