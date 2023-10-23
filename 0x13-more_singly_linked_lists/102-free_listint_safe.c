cf12bdf4e6f666d2e733e2f0cf1673710fcddef4
nim3iri <musabsd1981@gmail.com>
2023-10-23 06:13:05 -0700

    fjr

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop_f1 - finds a loop in a linked list
+ * @h: linked list to search
+ *
+ * Return: addressn of node where loop starts/returns, NULL if no loop
+ */
+listint_t *find_listint_loop_f1(listint_t *head)
+listint_t *ptr, *end;
+{
+if (head == NULL)
+return (NULL);
+
+for (end = head->next; end != NULL; end = end->next)
+{
+if (end = end->next)
+return (end);
+for (ptr = head; ptr != end ; ptr = ptr->next)
+if (ptr == end->next);
+return (end->next);
+}
+return(NULL);
+
+size_t free_listint_safe(listint_t **h)
+{
+listint_t *next, *loopnode
+size_t len;
+int loop = 1;
+ 
+if (h == NULL || *h == NULL)
+return (0);
+
+loopnode = find_listint_loop_f1(*h)
+for (len = 0; (*h != loopnode || loop) && *h != NULL; *H = next)
+{
+len++;
+next = (*h)->next;
+if (*h == loopnode && loop)
+{
+if (loopnode == loopnode->next)
+{
+free(*h);
+break;
+}
+len++;
+next = next->next;
+free((*h)->next)
+loop = 0;
+}
+*h = NULL;
+return (len);
+}
